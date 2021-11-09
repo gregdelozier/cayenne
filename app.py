74c14ba4b54faacadf338eee43b676fe908fbc43
gregdelozier <gregdelozier@users.noreply.github.com>
2021-11-09 17:14:48 -0500

    added hello route

--- app.py
+++ app.py
@@ -2,8 +2,12 @@
 # A very simple Flask Hello World app for you to get started with...
 
 from flask import Flask
+from flask import render_template
 
-app = Flask(__name__)
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name)
 
 @app.route('/')
 def get_index():
